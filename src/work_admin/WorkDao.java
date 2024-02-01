63f15d274e51491c340bbce220fcc815d3b30f4a
seed-yoo <lukdown10@naver.com>
2024-02-02 00:04:39 +0900

    ..

---
+++ src/work_admin/WorkDao.java
@@ -0,0 +1,377 @@
+package work_admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkDao {
+
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/work_db";
+	private String id = "work";
+	private String pw = "work";
+
+	// 메소드- 일반
+
+	private void getConnection() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver); // 위에 생성자로 올려주고 변수명으로 넣어줌
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		}
+
+	} // getConnection()끝
+
+	private void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}// close()끝
+
+	// 직원리스트(근태현황)
+	public List<WorkVo> eplyList() {
+		this.getConnection();
+
+		List<WorkVo> eplyList = new ArrayList<WorkVo>();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// -sql문 준비
+			String query = "";
+			query += " select 	u.user_id ";
+			query += " 			,user_name ";
+			query += "			,department_name ";
+			query += "			,count(case when state = '근무' then 1 end) as '근무' ";
+			query += "			,count(case when state = '휴가' then 1 end) as '휴가' ";
+			query += "			,count(case when state = '병가' then 1 end) as '병가' ";
+			query += "			,count(case when state = '무단결근' then 1 end) as '무단결근' ";
+			query += " from user u left join work w ";
+			query += " 				on u.user_id = w.user_id ";
+			query += " 				left join department d ";
+			query += " 				on u.department_id = d.department_id ";
+			query += " group by u.user_id; ";
+
+			// -바인딩
+			pstmt = conn.prepareStatement(query);
+
+			// -실행
+			rs = pstmt.executeQuery();
+
+			// 결과처리
+			while (rs.next()) {
+				String id = rs.getString("user_id");
+				String name = rs.getString("user_name");
+				String dptmtName = rs.getString("department_name");
+				int cWork = rs.getInt("근무");
+				int cRest = rs.getInt("휴가");
+				int cSick = rs.getInt("병가");
+				int cRun = rs.getInt("무단결근");
+
+				WorkVo workVo = new WorkVo(id, name, dptmtName, cWork, cRest, cSick, cRun);
+				eplyList.add(workVo);
+
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		this.close();
+		return eplyList;
+
+	}
+
+	// 근태현황 수정
+	// work리스트
+	public List<WorkVo> workList(String userId) {
+
+		// 리스트 준비
+		List<WorkVo> workList = new ArrayList<WorkVo>();
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// - sql문 준비
+			String query = "";
+			query += "select user_id, ";
+			query += "       state, ";
+			query += "       date_format(work_date, '%Y-%m-%d') work_date ";
+			query += " from work ";
+			query += " where user_id=? ";
+
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, userId);
+
+			// - 실행
+			rs = pstmt.executeQuery();
+
+			// 4.결과처리
+			while (rs.next()) {
+				String id = rs.getString("user_id");
+				String state = rs.getString("state");
+				String work_date = rs.getString("work_date");
+
+				WorkVo workVo = new WorkVo(id, state, work_date);
+
+				workList.add(workVo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		this.close();
+
+		return workList;
+	}
+
+	// work수정
+	public int workUpdate(String userId, String workDate, String workState) {
+		int count = -1;
+
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// 2. Connection 얻어오기
+			String url = "jdbc:mysql://127.0.0.1:3306/work_db";
+			conn = DriverManager.getConnection(url, "work", "work");
+			
+			
+			
+			
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " update work set state=? where work_date=? and user_id=? ";
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, workState);
+			pstmt.setString(2, workDate);
+			pstmt.setString(3, userId);
+
+			// - 실행
+			count = pstmt.executeUpdate();
+			// 4.결과처리
+			//System.out.println(count + "건 수정 되었습니다.");
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		this.close();
+		return count;
+	}
+
+	// 부서 리스트
+	public List<DepartmentVo> departmentList() {
+
+		// 리스트 준비
+		List<DepartmentVo> departmentList = new ArrayList<DepartmentVo>();
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// - sql문 준비
+			String query = "";
+			query += " select department_id, ";
+			query += "        user_id, ";
+			query += "        department_name ";
+			query += " from department ";
+
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+
+			// - 실행
+			rs = pstmt.executeQuery();
+
+			// 검색결과에서 데이터 꺼내기
+			while (rs.next()) {
+
+				int department_id = rs.getInt("department_id");
+				String user_id = rs.getString("user_id");
+				String department_name = rs.getString("department_name");
+
+				// Vo묶기
+				DepartmentVo departmentVo = new DepartmentVo(department_id, user_id, department_name);
+
+				// 리스트에 추가
+				departmentList.add(departmentVo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		this.close();
+
+		return departmentList;
+	}
+
+	// 부서 등록
+	public int departmentInsert(DepartmentVo departmentVo) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// - sql문 준비
+			String query = "";
+			query += " insert into department ";
+			query += " value (?, ?, ?) ";
+
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, departmentVo.getDepartment_id());
+			pstmt.setString(2, departmentVo.getUser_id());
+			pstmt.setString(3, departmentVo.getDepartment_name());
+
+			// - 실행
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 등록 되었습니다.");
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		this.close();
+
+		return count;
+	}
+
+	// 부서 수정
+	public int departmentUpdate(DepartmentVo departmentVo) {
+
+		int count = -1;
+
+		this.getConnection();
+		// 0. import java.sql.*;
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// - sql문 준비
+
+			String query = "";
+			query += " update department ";
+			query += " set user_id = ?, ";
+			query += " 	   department_name = ? ";
+			query += " where department_id = ? ";
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, departmentVo.getUser_id());
+			pstmt.setString(2, departmentVo.getDepartment_name());
+			pstmt.setInt(3, departmentVo.getDepartment_id());
+
+			// - 실행
+			// count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			// System.out.println(count + "건 수정 되었습니다.");
+			// System.out.println("수정완료");
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+		this.close();
+		return count;
+
+	}
+
+	// 부서 삭제
+	public int departmentDelete(DepartmentVo departmentVo) {
+
+		int count = -1;
+
+		this.getConnection();
+		// 0. import java.sql.*;
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			// - sql문 준비
+			String query = "";
+			query += " delete from department ";
+			query += " where department_id = ? ";
+			// - 바인딩
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, departmentVo.getDepartment_id());
+
+			// - 실행
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			// System.out.println(count + "건 수정 되었습니다.");
+			System.out.println("삭제완료");
+
+		} catch (SQLException e) {
+			System.out.println("error: " + e);
+		} catch (NullPointerException e) {
+			System.out.println("부서번호를 확인해주세요");
+		}
+		this.close();
+		return count;
+
+	}
+
+	/*
+	 * public int departmentFinsert(int n) {
+	 * 
+	 * int count = -1;
+	 * 
+	 * this.getConnection(); // 0. import java.sql.*;
+	 * 
+	 * try {
+	 * 
+	 * // 3. SQL문 준비 / 바인딩 / 실행 // - sql문 준비
+	 * 
+	 * String query = ""; query += " set foreign_key_checks = 0 ";
+	 * 
+	 * // - 바인딩 pstmt = conn.prepareStatement(query); //pstmt.setInt(1, n);
+	 * 
+	 * 
+	 * // - 실행 count = pstmt.executeUpdate();
+	 * 
+	 * // 4.결과처리 // System.out.println(count + "건 수정 되었습니다."); //
+	 * System.out.println("수정완료");
+	 * 
+	 * } catch (SQLException e) { System.out.println("error:" + e); } this.close();
+	 * return count;
+	 * 
+	 * }
+	 * 
+	 */
+}
---
+++ src/work_admin/WorkVo.java
@@ -0,0 +1,153 @@
+package work_admin;
+
+public class WorkVo {
+
+	// 필드
+	private int work_id;
+	private String user_id;
+	private String state;
+	private String work_date;
+	
+	private String user_name;
+	private String department_name;
+	private int cwork;
+	private int crest;
+	private int csick;
+	private int crun;
+	
+	// 생성자
+	public WorkVo() {
+		super();
+	}
+
+	public WorkVo(int work_id, String user_id, String state, String work_date) {
+		super();
+		this.work_id = work_id;
+		this.user_id = user_id;
+		this.state = state;
+		this.work_date = work_date;
+	}
+	
+	public WorkVo(String user_id, String state, String work_date) {
+		super();
+		this.user_id = user_id;
+		this.state = state;
+		this.work_date = work_date;
+	}
+	
+
+
+	public WorkVo(String user_id, String user_name, String department_name,
+			int cwork, int crest, int csick, int crun) {
+		this.user_id = user_id;
+		this.user_name = user_name;
+		this.department_name = department_name;
+		this.cwork = cwork;
+		this.crest = crest;
+		this.csick = csick;
+		this.crun = crun;
+	}
+
+	// 메소드 - getter/setter
+	public int getWork_id() {
+		return work_id;
+	}
+
+	public void setWork_id(int work_id) {
+		this.work_id = work_id;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getWork_date() {
+		return work_date;
+	}
+
+	public void setWork_date(String work_date) {
+		this.work_date = work_date;
+	}
+
+	public String getUser_name() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
+	
+	public int getCwork() {
+		return cwork;
+	}
+
+	public void setCwork(int cwork) {
+		this.cwork = cwork;
+	}
+
+	public int getCrest() {
+		return crest;
+	}
+
+	public void setCrest(int crest) {
+		this.crest = crest;
+	}
+
+	public int getCsick() {
+		return csick;
+	}
+
+	public void setCsick(int csick) {
+		this.csick = csick;
+	}
+
+	public int getCrun() {
+		return crun;
+	}
+
+	public void setCrun(int crun) {
+		this.crun = crun;
+	}
+
+
+
+	// 메소드 - 일반
+	@Override
+	public String toString() {
+		return "WorkVo [work_id=" + work_id + ", user_id=" + user_id + ", state=" + state + ", work_date=" + work_date
+				+ ", user_name=" + user_name + ", department_name=" + department_name + ", cwork=" + cwork + ", crest="
+				+ crest + ", csick=" + csick + ", crun=" + crun + "]";
+	}
+//	@Override
+//	public String toString() {
+//		return "WorkVo [work_id=" + work_id + ", user_id=" + user_id + ", state=" + state + ", work_date=" + work_date
+//				+ "]";
+//	}
+	
+	
+	
+	
+	
+	
+	
+}
